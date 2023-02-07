ecab8cded9f0195aec1ec5c875cac75e696752b3
Karen Yep <karenyeplee@gmail.com>
2023-02-07 12:06:46 -0600

    floating button

--- src/components/CreateFlitButton.vue
+++ src/components/CreateFlitButton.vue
@@ -21,9 +21,15 @@ export default defineComponent({
 </script>
 <style scoped>
 .btn{
-    margin: 15px;
-    position: fixed;
-    bottom: 0;
-    right: 0;
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#0C9;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
 }
 </style>
