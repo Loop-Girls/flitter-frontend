486916aa1059c0080ae95430f8bf47b52fbef8a7
CristinaMercedesNunez <cristinamnunez@hotmail.com>
2023-02-13 01:22:02 +0100

    logIn

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Hola Usuario!
+    </div>
+
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default{
+    name: "Dashboard",
+    data()
+    component:{
+        NavBar,
+    },
+  
+
+}
+
+</script>
